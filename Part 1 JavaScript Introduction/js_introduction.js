8fff1b99be4b15ee5105c99f61fda968240c5fd1
Ranbir <ranbir.webdev@gmail.com>
2026-07-24 15:54:04 +0600

    add js introduction

---
+++ Part 1 JavaScript Introduction/js_introduction.js
@@ -0,0 +1,152 @@
+/****************************************************************
+                    JavaScript Introduction
+****************************************************************/
+
+/*
+JavaScript holo ekta High Level Programming Language.
+
+Eta diye amra website ke Dynamic korte pari.
+
+HTML = Structure
+CSS = Design
+JavaScript = Functionality
+
+JavaScript chara website sudhu static thakbe.
+JavaScript add korle website interactive hoye jay.
+*/
+
+
+
+
+// HTML + CSS + JavaScript Relationship
+/*
+Example:
+
+HTML -> Bari toiri kore.
+CSS -> Bari ke sundor kore.
+JavaScript -> Barite electricity, fan, light, door sob kaj kore.
+
+Tai JavaScript ke website er brain bola hoy.
+*/
+
+
+
+
+// JavaScript দিয়ে কী কী করা যায়?
+/*
+JavaScript diye amra—
+
+✔ Button click handle korte pari
+✔ Form Validation korte pari
+✔ Calculator banate pari
+✔ To Do App banate pari
+✔ Weather App banate pari
+✔ Chat App banate pari
+✔ Game banate pari
+✔ Full Stack Website banate pari
+✔ Mobile App banate pari (React Native)
+✔ Desktop App banate pari (Electron)
+✔ Backend Server banate pari (Node.js)
+*/
+// Example 1
+// Console e message dekhabe
+
+console.log("Hello JavaScript");
+
+// Example 2
+// Variable declare kora
+
+let name = "Ranbir";
+
+console.log(name);
+
+
+
+
+// JavaScript-এর বৈশিষ্ট্য (Features)
+/*
+JavaScript er kichu important feature
+
+1. Easy to Learn
+2. Lightweight
+3. High Level Language
+4. Dynamic Language
+5. Interpreted Language
+6. Object Oriented
+7. Prototype Based
+8. Cross Platform
+9. Event Driven
+10. Asynchronous Programming Support
+*/
+
+
+
+
+// JavaScript কোথায় রান করে?
+/*
+JavaScript mainly dui jaygay run kore.
+
+1. Browser
+
+Example:
+Google Chrome
+Microsoft Edge
+Firefox
+Safari
+
+2. Node.js
+
+Eta diye browser charao computer e JavaScript run kora jay.
+*/
+
+
+
+// Real Life Example
+/*
+Facebook
+
+HTML -> Layout
+
+CSS -> Design
+
+JavaScript ->
+Like Button
+Comment
+Messenger
+Notification
+Dark Mode
+*/
+
+
+
+
+
+// JavaScript কী ধরনের Language?
+/*
+JavaScript holo—
+
+Programming Language
+
+High Level Language
+
+Interpreted Language
+
+Dynamic Language
+
+Multi Paradigm Language
+*/
+
+
+
+// Interview Note 
+/*
+Question:
+
+JavaScript ki?
+
+Answer:
+
+JavaScript holo ekta High Level, Dynamic,
+Interpreted Programming Language,
+ja diye website interactive o dynamic kora hoy.
+*/
